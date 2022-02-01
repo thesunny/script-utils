dae94f06bfc081023d503c4908fa1b12561c7a7e
Sunny Hirai <thesunny@gmail.com>
2022-01-31 18:01:48 -0800

    add exit

--- src/index.ts
+++ src/index.ts
@@ -358,3 +358,19 @@ export function spawn(
   }
   return result
 }
+
+/**
+ * Add a blank line to make output cleaner.
+ *
+ * NOTE:
+ *
+ * We elect to exit with `process.exit(1)` which displays an ugly error
+ * in order to communicate that the code has exited in a state we don't want.
+ *
+ * Even though we show the error messages, the ugliness of the different colors
+ * is a nice visual indicator that we need to pay attention.
+ */
+export function exit() {
+  console.log("")
+  process.exit(1)
+}
