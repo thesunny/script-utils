51ed8b4394e249358889c95efdee0d7b1c2511af
Sunny Hirai <thesunny@gmail.com>
2022-06-18 12:16:09 -0700

    Switch log.line to log.progress

--- src/log.ts
+++ src/log.ts
@@ -16,17 +16,17 @@ function Log(ch: chalk.Chalk) {
 const log = {
   title: Log(chalk.yellowBright),
   heading: Log(chalk.yellowBright),
-  line: Log(chalk.greenBright),
+  progress: Log(chalk.greenBright),
   task: Log(chalk.yellow),
   message: Log(chalk.hex("#c0c0a0")),
   alert: Log(chalk.hex("FF9010")),
-  pass: Log(chalk.greenBright),
+  pass: Log(chalk.green),
   skip: Log(chalk.hex("#006000")),
   fail: Log(chalk.redBright),
 }
 
-export function line(text: string) {
-  log.line(text)
+export function progress(text: string) {
+  log.progress(text)
 }
 
 export function task(text: string) {
